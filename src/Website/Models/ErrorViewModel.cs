d2893b7fc3dbc25e7ceacc676ebedeb569791263
Spec <dawidjachowicz91@gmail.com>
2022-06-16 09:13:39 +0200

    Add missing ErrorViewModel

--- src/Website/Models/ErrorViewModel.cs
+++ src/Website/Models/ErrorViewModel.cs
@@ -2,6 +2,8 @@ namespace Website.Models;
 
 public class ErrorViewModel
 {
+    public string? ErrorCode { get; set; }
+
     public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
