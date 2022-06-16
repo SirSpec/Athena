86d6c816465da79529424b385816035092742d83
Spec <dawidjachowicz91@gmail.com>
2022-06-16 09:12:42 +0200

    Add Error controller and view

---
+++ src/Website/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Website.Models;
+
+namespace Website.Controllers;
+
+[Route("{controller}")]
+public class ErrorController : Controller
+{
+    [Route("{code}")]
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Index(string code)
+    {
+        return View(new ErrorViewModel
+        {
+            ErrorCode = code,
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+        });
+    }
+}
