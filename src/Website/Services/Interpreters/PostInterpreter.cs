c752896ec72724c4d912883f6573d4b1c3a2ae5c
Spec <dawid.spec.jachowicz@gmail.com>
2022-06-16 09:05:48 +0200

    Add PostInterpreter

---
+++ src/Website/Services/Interpreters/PostInterpreter.cs
@@ -0,0 +1,21 @@
+using System.Text.RegularExpressions;
+
+namespace Website.Services.Interpreters;
+
+public class PostInterpreter : IPostInterpreter
+{
+    private const string RegexPattern = @"(?<={{{0}}})[\s\S]*(?={{{0}}})";
+
+    public string Interpret(string postData, string token)
+    {
+        var regex = new Regex(GetRegexPattern(token), RegexOptions.Multiline);
+        var match = regex.Match(postData);
+
+        return match.Success
+            ? match.Value.Trim()
+            : string.Empty;
+    }
+
+    private string GetRegexPattern(string token) =>
+        string.Format(RegexPattern, token);
+}
