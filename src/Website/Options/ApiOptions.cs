6a2535227512adeb029c9c15d83befbf730b2f46
Spec <dawid.spec.jachowicz@gmail.com>
2022-06-16 09:01:13 +0200

    Add ApiOptions

--- src/Website/Options/ApiOptions.cs
+++ src/Website/Options/ApiOptions.cs
@@ -2,7 +2,7 @@ namespace Website.Options;
 
 public class ApiOptions
 {
-    public string? BaseAddress { get; set; }
-    public string? ContentPath { get; set; }
-    public string? AccessToken { get; set; }
+    public string PostApiUrl { get; set; } = string.Empty;
+    public string RawDataApiUrl { get; set; } = string.Empty;
+    public string AccessToken { get; set; } = string.Empty;
 }
