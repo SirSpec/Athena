76a24a557c0e595b2c74fd263b16ee99cd5e38d2
Spec <dawid.spec.jachowicz@gmail.com>
2022-06-16 09:19:34 +0200

    Refactor PostTeaserViewModel - Remove Id and Url properties - Add Name property

--- src/Website/Models/PostTeaserViewModel.cs
+++ src/Website/Models/PostTeaserViewModel.cs
@@ -4,9 +4,8 @@ namespace Website.Models;
 
 public class PostTeaserViewModel
 {
-    public string Id { get; init; } = string.Empty;
+    public HtmlString Name { get; init; } = HtmlString.Empty;
     public HtmlString PublishingDate { get; init; } = HtmlString.Empty;
     public HtmlString Title { get; init; } = HtmlString.Empty;
     public HtmlString Description { get; init; } = HtmlString.Empty;
-    public HtmlString Url { get; init; } = HtmlString.Empty;
 }
