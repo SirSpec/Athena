e0abafab64cb31f344c9d8a8d9e3476ce7aa18ad
Spec <dawid.spec.jachowicz@gmail.com>
2022-06-16 09:14:32 +0200

    Add Post controller and view

---
+++ src/Website/Controllers/PostController.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Website.Services;
+
+namespace Website.Controllers;
+
+[Route("{controller}")]
+public class PostController : Controller
+{
+    private readonly ILogger<PostController> _logger;
+    private readonly IPostService _postService;
+
+    public PostController(ILogger<PostController> logger, IPostService postService)
+    {
+        _logger = logger;
+        _postService = postService;
+    }
+
+    [Route("{name}")]
+    public async Task<IActionResult> Index([Required] string name)
+    {
+        var post = await _postService.GetPostViewModelAsync(name);
+        return View(post);
+    }
+}
