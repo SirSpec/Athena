5a8809b7cc558e3d7e6beb933e71391f0f60a1ab
Spec <dawid.spec.jachowicz@gmail.com>
2022-06-16 09:22:33 +0200

    Improve Program configuration - Add Cors configuration - Adjust Polly policies - Configure Error page redirects

--- src/Website/Program.cs
+++ src/Website/Program.cs
@@ -1,6 +1,6 @@
 using Website.Options;
 using Website.Policies;
-using Website.Services;
+using Website.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,40 +12,49 @@ builder.Services.AddOptions<ApiOptions>()
 builder.Services.AddControllersWithViews();
 
 builder.Services
-    .AddHttpClient<IPostService, PostService>(configureClient =>
+    .AddHttpClient<IPostRepository, PostRepository>(configureClient =>
     {
-        var apiOptions = builder.Configuration
-            .GetSection(nameof(ApiOptions))
-            .Get<ApiOptions>();
-
-        configureClient.BaseAddress = new Uri(apiOptions.BaseAddress ?? throw new ArgumentNullException());
         configureClient.DefaultRequestHeaders.Add("User-Agent", "request");
         configureClient.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
 
         if (builder.Environment.IsDevelopment())
+        {
+            var apiOptions = builder.Configuration
+                .GetSection(nameof(ApiOptions))
+                .Get<ApiOptions>();
+
             configureClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiOptions.AccessToken}");
+        }
     })
     .SetHandlerLifetime(TimeSpan.FromMinutes(5))
-    .AddPolicyHandler(PostServicePolicyFactory.GetRetryPolicy(builder.Services));
+    .AddPolicyHandler(PostServicePolicyFactory.GetRetryPolicy(builder.Services))
+    .AddPolicyHandler(PostServicePolicyFactory.GetCircuitBreakerPolicy(builder.Services));
+
+builder.Services.AddDependencies();
 
 var app = builder.Build();
 
 if (app.Environment.IsProduction())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/error");
     app.UseHsts();
 }
 
+app.UseStatusCodePagesWithRedirects("/error/{0}");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
-app.UseCors();
+app.UseCors(policy =>
+{
+    var apiOptions = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
+    policy.WithOrigins(apiOptions);
+});
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=home}/{action=index}/{id?}");
 
 app.Run();
