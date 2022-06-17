85cda0db68a58ecc76daa7b4691fed0ca02d2004
Spec <dawidjachowicz91@gmail.com>
2022-06-17 09:08:08 +0200

    Improve PostRepository and refactor ContentItem - Add Exceptions handling and Url validation

--- src/Website/Repositories/PostRepository.cs
+++ src/Website/Repositories/PostRepository.cs
@@ -20,11 +20,33 @@ public class PostRepository : IPostRepository
         _httpClient = httpClient;
     }
 
-    public async Task<IEnumerable<ContentItem>> GetPostTeasersAsync() =>
-        await _httpClient.GetFromJsonAsync<IEnumerable<ContentItem>>(
-                new Uri(_apiOptions.PostApiUrl))
-            ?? Enumerable.Empty<ContentItem>();
+    public async Task<IEnumerable<ContentItem>> GetPostTeasersAsync()
+    {
+        try
+        {
+            return Uri.TryCreate(_apiOptions.PostApiUrl, UriKind.RelativeOrAbsolute, out var uri)
+                ? await _httpClient.GetFromJsonAsync<IEnumerable<ContentItem>>(uri) ?? Enumerable.Empty<ContentItem>()
+                : Enumerable.Empty<ContentItem>();
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "Could not get posts.");
+            return Enumerable.Empty<ContentItem>();
+        }
+    }
 
-    public async Task<string> GetPostDataAsync(string path) =>
-        await _httpClient.GetStringAsync(new Uri($"{_apiOptions.RawDataApiUrl}{path}"));
+    public async Task<string> GetPostDataAsync(string path)
+    {
+        try
+        {
+            return Uri.TryCreate($"{_apiOptions.RawDataApiUrl}{path}", UriKind.RelativeOrAbsolute, out var uri)
+                ? await _httpClient.GetStringAsync(uri)
+                : string.Empty;
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "Could not get post content.");
+            return string.Empty;
+        }
+    }
 }
