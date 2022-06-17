f16a2a2a045c7dd2e1f484a601c407f737b20519
Spec <dawidjachowicz91@gmail.com>
2022-06-17 09:06:22 +0200

    Add Empty property to PostViewModel

--- src/Website/Models/PostViewModel.cs
+++ src/Website/Models/PostViewModel.cs
@@ -4,6 +4,7 @@ namespace Website.Models;
 
 public class PostViewModel
 {
+    public static readonly PostViewModel Empty = new PostViewModel();
     public HtmlString PublishingDate { get; init; } = HtmlString.Empty;
     public HtmlString Title { get; init; } = HtmlString.Empty;
     public HtmlString Body { get; init; } = HtmlString.Empty;
