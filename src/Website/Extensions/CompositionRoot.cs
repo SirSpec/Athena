4ad62690b39d504dcefe81b70c76e8be9893609d
Spec <dawidjachowicz91@gmail.com>
2022-06-17 09:11:23 +0200

    Add AddHttpClient to CompositionRoot extensions

--- src/Website/Extensions/CompositionRoot.cs
+++ src/Website/Extensions/CompositionRoot.cs
@@ -1,3 +1,6 @@
+using Website.Options;
+using Website.Policies;
+using Website.Repositories;
 using Website.Services;
 using Website.Services.Interpreters;
 
@@ -9,4 +12,25 @@ public static class CompositionRoot
         services
             .AddScoped<IPostInterpreter, PostInterpreter>()
             .AddScoped<IPostService, PostService>();
+
+    public static IServiceCollection AddHttpClient(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddHttpClient<IPostRepository, PostRepository>(configureClient =>
+            {
+                configureClient.DefaultRequestHeaders.Add("User-Agent", "request");
+                configureClient.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+
+                var apiOptions = configuration
+                    .GetSection(nameof(ApiOptions))
+                    .Get<ApiOptions>();
+
+                configureClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiOptions.AccessToken}");
+            })
+            .SetHandlerLifetime(TimeSpan.FromMinutes(5))
+            .AddPolicyHandler(PostApiPolicyFactory.GetRetryPolicy(services))
+            .AddPolicyHandler(PostApiPolicyFactory.GetCircuitBreakerPolicy(services));
+
+        return services;
+    }
 }
