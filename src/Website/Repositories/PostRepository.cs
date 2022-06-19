b17f0ff74e4672a7a154cd779bb93b88ad159873
Spec <dawidjachowicz91@gmail.com>
2022-06-19 11:07:11 +0200

    Add Post name validation in PostRepository

--- src/Website/Repositories/PostRepository.cs
+++ src/Website/Repositories/PostRepository.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Options;
 using Website.Options;
 using Website.Repositories.Models;
+using Website.Services.Validators;
 
 namespace Website.Repositories;
 
@@ -9,15 +10,18 @@ public class PostRepository : IPostRepository
     private readonly ILogger<PostRepository> _logger;
     private readonly ApiOptions _apiOptions;
     private readonly HttpClient _httpClient;
+    private readonly IPostValidator _postValidator;
 
     public PostRepository(
         ILogger<PostRepository> logger,
         IOptions<ApiOptions> apiOptions,
-        HttpClient httpClient)
+        HttpClient httpClient,
+        IPostValidator postValidator)
     {
         _logger = logger;
         _apiOptions = apiOptions.Value;
         _httpClient = httpClient;
+        _postValidator = postValidator;
     }
 
     public async Task<IEnumerable<ContentItem>> GetPostTeasersAsync()
@@ -25,7 +29,8 @@ public class PostRepository : IPostRepository
         try
         {
             return Uri.TryCreate(_apiOptions.PostApiUrl, UriKind.RelativeOrAbsolute, out var uri)
-                ? await _httpClient.GetFromJsonAsync<IEnumerable<ContentItem>>(uri) ?? Enumerable.Empty<ContentItem>()
+                ? (await _httpClient.GetFromJsonAsync<IEnumerable<ContentItem>>(uri) ?? Enumerable.Empty<ContentItem>())
+                    .Where(item => _postValidator.IsNameValid(item.Name))
                 : Enumerable.Empty<ContentItem>();
         }
         catch (Exception exception)
@@ -35,11 +40,11 @@ public class PostRepository : IPostRepository
         }
     }
 
-    public async Task<string> GetPostDataAsync(string path)
+    public async Task<string> GetPostDataAsync(string name)
     {
         try
         {
-            return Uri.TryCreate($"{_apiOptions.RawDataApiUrl}{path}", UriKind.RelativeOrAbsolute, out var uri)
+            return _postValidator.IsNameValid(name) && Uri.TryCreate($"{_apiOptions.RawDataApiUrl}{name}", UriKind.RelativeOrAbsolute, out var uri)
                 ? await _httpClient.GetStringAsync(uri)
                 : string.Empty;
         }
