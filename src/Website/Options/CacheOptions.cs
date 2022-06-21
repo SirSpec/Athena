572dc5970b1a78a05835dd7a9bdc7dea60c9afb6
Spec <dawid.spec.jachowicz@gmail.com>
2022-06-21 14:09:12 +0200

    Add CacheOptions class

---
+++ src/Website/Options/CacheOptions.cs
@@ -0,0 +1,7 @@
+namespace Website.Options;
+
+public class CacheOptions
+{
+    public int AbsoluteExpirationRelativeToNowOk { get; set; }
+    public int AbsoluteExpirationRelativeToNowNotFound { get; set; }
+}
