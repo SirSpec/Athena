03b36877d244bee1c267957ed817e4644b6eeb0c
Spec <dawidjachowicz91@gmail.com>
2022-06-23 07:59:55 +0200

    Add AddAppConfiguration extension method

---
+++ src/Website/Extensions/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,23 @@
+using Website.Options;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ConfigurationBuilderExtensions
+{
+    public static IConfigurationBuilder AddAppConfiguration(this IConfigurationBuilder configurationBuilder)
+    {
+        var configuration = configurationBuilder.Build();
+        var connectionString = configuration.GetConnectionString("AppConfigurationService");
+        return string.IsNullOrWhiteSpace(connectionString) is false
+            ? configurationBuilder.AddAzureAppConfiguration(options =>
+            {
+                var cacheExpiration = configuration.GetValue<int>("AppConfigurationOptions:CacheExpiration");
+                options.Connect(connectionString).ConfigureRefresh(
+                    configure => configure
+                        .Register(nameof(CacheOptions))
+                        .SetCacheExpiration(TimeSpan.FromHours(cacheExpiration))
+                );
+            })
+            : configurationBuilder;
+    }
+}
