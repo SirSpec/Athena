ed589919b26cc4ad1ceceddea9ce6bb6f6db67b9
Spec <dawidjachowicz91@gmail.com>
2022-06-25 18:40:42 +0200

    Add API policy options to ApiOptions

--- src/Website/Options/ApiOptions.cs
+++ src/Website/Options/ApiOptions.cs
@@ -6,4 +6,8 @@ public class ApiOptions
     public string RawDataApiUrl { get; set; } = string.Empty;
     public string AccessToken { get; set; } = string.Empty;
     public int HttpMessageHandlerLifeTime { get; set; }
+    public int RetryCount { get; set; }
+    public int BaseRetryDelay { get; set; }
+    public int HandledEventsAllowedBeforeBreaking { get; set; }
+    public int DurationOfBreak { get; set; }
 }
