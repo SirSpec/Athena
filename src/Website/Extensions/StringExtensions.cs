e9af4196a06a84afc22676a9ac510850c59a8a99
Spec <dawidjachowicz91@gmail.com>
2022-07-02 21:45:29 +0200

    Add ToAbsoluteUri extension method

--- src/Website/Extensions/StringExtensions.cs
+++ src/Website/Extensions/StringExtensions.cs
@@ -6,4 +6,9 @@ public static class StringExtensions
 {
     public static HtmlString ToHtmlString(this string @string) =>
         new HtmlString(@string);
+
+    public static Uri ToAbsoluteUri(this string url) =>
+        Uri.TryCreate(url, UriKind.Absolute, out var uri)
+            ? uri
+            : throw new ArgumentException($"Url:{url} cannot be used to construct an absolute URI.");
 }
