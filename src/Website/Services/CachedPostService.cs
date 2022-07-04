926920d7e7e94041cd30e913bae5d39dd91d3266
Spec <dawidjachowicz91@gmail.com>
2022-07-04 10:16:11 +0200

    Add CachedPostService

---
+++ src/Website/Services/CachedPostService.cs
@@ -0,0 +1,43 @@
+using Website.Models;
+using Microsoft.Extensions.Caching.Memory;
+using Website.Options;
+using Microsoft.Extensions.Options;
+
+namespace Website.Services;
+
+public class CachedPostService : IPostService
+{
+    private readonly CacheOptions _cacheOptions;
+    private readonly IMemoryCache _memoryCache;
+    private readonly IPostService _postService;
+
+    public CachedPostService(
+        IOptionsSnapshot<CacheOptions> cacheOptions,
+        IMemoryCache memoryCache,
+        IPostService postService)
+    {
+        _cacheOptions = cacheOptions.Value;
+        _memoryCache = memoryCache;
+        _postService = postService;
+    }
+
+    public async Task<HomeViewModel> GetHomeViewModelAsync() =>
+        await _memoryCache.GetOrCreateAsync<HomeViewModel>(
+            "HomePage",
+            async cacheEntry =>
+            {
+                var teasers = await _postService.GetHomeViewModelAsync();
+                cacheEntry.AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(_cacheOptions.PostDataTimeToLiveInHours);
+                return teasers;
+            });
+
+    public async Task<PostViewModel> GetPostViewModelAsync(string name) =>
+        await _memoryCache.GetOrCreateAsync<PostViewModel>(
+            name,
+            async cacheEntry =>
+            {
+                var model = await _postService.GetPostViewModelAsync(name);
+                cacheEntry.AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(_cacheOptions.PostDataTimeToLiveInHours);
+                return model;
+            });
+}
