197dac31cbcdfb3f3da37da5a5e0490b0fc85a57
Spec <dawid.spec.jachowicz@gmail.com>
2022-07-04 10:11:54 +0200

    Refactor Post repository - Use PostName and Post value objects - Remove exception handling and logging - Use ToAbsoluteUri extension method

--- src/Website/Repositories/PostRepository.cs
+++ src/Website/Repositories/PostRepository.cs
@@ -1,57 +1,39 @@
+using Website.Extensions;
 using Microsoft.Extensions.Options;
 using Website.Options;
 using Website.Repositories.Models;
-using Website.Services.Validators;
+using Website.Domain.ValueObjects;
 
 namespace Website.Repositories;
 
 public class PostRepository : IPostRepository
 {
-    private readonly ILogger<PostRepository> _logger;
     private readonly ApiOptions _apiOptions;
     private readonly HttpClient _httpClient;
-    private readonly IPostValidator _postValidator;
 
     public PostRepository(
-        ILogger<PostRepository> logger,
         IOptions<ApiOptions> apiOptions,
-        HttpClient httpClient,
-        IPostValidator postValidator)
+        HttpClient httpClient)
     {
-        _logger = logger;
         _apiOptions = apiOptions.Value;
         _httpClient = httpClient;
-        _postValidator = postValidator;
     }
 
-    public async Task<IEnumerable<ContentItem>> GetPostTeasersAsync()
+    public async Task<IEnumerable<PostName>> GetPostNamesAsync()
     {
-        try
-        {
-            return Uri.TryCreate(_apiOptions.PostApiUrl, UriKind.RelativeOrAbsolute, out var uri)
-                ? (await _httpClient.GetFromJsonAsync<IEnumerable<ContentItem>>(uri) ?? Enumerable.Empty<ContentItem>())
-                    .Where(item => _postValidator.IsNameValid(item.Name))
-                : Enumerable.Empty<ContentItem>();
-        }
-        catch (Exception exception)
-        {
-            _logger.LogError(exception, "Could not get posts.");
-            return Enumerable.Empty<ContentItem>();
-        }
+        var uri = _apiOptions.PostApiUrl.ToAbsoluteUri();
+        var contentItems = await _httpClient.GetFromJsonAsync<IEnumerable<ContentItem>>(uri);
+
+        return contentItems?
+            .Where(contentItem => PostName.IsNameValid(contentItem.Name))
+            .Select(contentItem => new PostName(contentItem.Name)) ?? Enumerable.Empty<PostName>();
     }
 
-    public async Task<string> GetPostDataAsync(string name)
+    public async Task<Post> GetPostAsync(PostName postName)
     {
-        try
-        {
-            return _postValidator.IsNameValid(name) && Uri.TryCreate($"{_apiOptions.RawDataApiUrl}{name}", UriKind.RelativeOrAbsolute, out var uri)
-                ? await _httpClient.GetStringAsync(uri)
-                : string.Empty;
-        }
-        catch (Exception exception)
-        {
-            _logger.LogError(exception, "Could not get post content.");
-            return string.Empty;
-        }
+        var uri = $"{_apiOptions.RawDataApiUrl}{postName.Value}".ToAbsoluteUri();
+        var postData = await _httpClient.GetStringAsync(uri);
+
+        return new Post(postName, postData);
     }
 }
