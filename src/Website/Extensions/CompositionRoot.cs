dcdc784a3df3b037d61b040c8dd01d263064a908
Spec <dawidjachowicz91@gmail.com>
2022-07-05 09:52:38 +0200

    Adjust CompositionRoot according to new configs

--- src/Website/Extensions/CompositionRoot.cs
+++ src/Website/Extensions/CompositionRoot.cs
@@ -1,10 +1,10 @@
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
 using Website.Options;
 using Website.Policies;
 using Website.Repositories;
 using Website.Services;
-using Website.Services.Interpreters;
 using Website.Services.Mappers;
-using Website.Services.Validators;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -12,10 +12,13 @@ public static class CompositionRoot
 {
     public static IServiceCollection AddDependencies(this IServiceCollection services) =>
         services
-            .AddScoped<IPostInterpreter, PostInterpreter>()
-            .AddScoped<IPostValidator, PostValidator>()
             .AddScoped<IPostMapper, PostMapper>()
-            .AddScoped<IPostService, PostService>()
+            .AddScoped<PostService>()
+            .AddScoped<IPostService, CachedPostService>(provider => new CachedPostService(
+                provider.GetRequiredService<IOptionsSnapshot<CacheOptions>>(),
+                provider.GetRequiredService<IMemoryCache>(),
+                provider.GetRequiredService<PostService>()
+            ))
             .AddScoped<IPostApiPolicyFactory, PostApiPolicyFactory>();
 
     public static IServiceCollection AddHttpClient(this IServiceCollection services, IConfiguration configuration)
@@ -40,7 +43,7 @@ public static class CompositionRoot
                 configureClient.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
                 configureClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
             })
-            .SetHandlerLifetime(TimeSpan.FromMinutes(apiOptions.HttpMessageHandlerLifeTime))
+            .SetHandlerLifetime(TimeSpan.FromMinutes(apiOptions.HttpMessageHandlerLifeTimeInMinutes))
             .AddPolicyHandler(policyFactory.GetRetryPolicy())
             .AddPolicyHandler(policyFactory.GetCircuitBreakerPolicy());
 
